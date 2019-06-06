6045de8ca92e6bf3c6974a5ac38e7f47a1faa5e6
luuk3616 <50212261+luuk3616@users.noreply.github.com>
2019-06-05 23:51:42 -0700

    Update hangman.c

--- hangman.c
+++ hangman.c
@@ -1,62 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-  // Get word to guess
-  char answer[128];
+int main() 
+    char word[20];			//Used to store the user word. Most words are less than 20 characters so the default size is 20.
+    char stars[20];			//A string full of * that are replaced upon the user entering the right letter of the word.
+    int counter = 0;			//A counter to use in the for loops.
+    int len;				//len is the length of the word entered.
+    char ch;				//ch is a temporary character variable.
+    int guessses=0;			//strikes counts how many wrong characters the user has guessed.
+    int flag = 0;			//The trigger variable serves as a flag in the for loops bellow.
+    int win=0;			//wincounter keeps track of the number of correct guesses.
+    int i;				//Another counter variable to be used in for loops.
+
   printf("Enter word to guess: ");
-  fflush(stdout);
+  len = strlen(word);	
   scanf(" %s", answer);
 
-  // Set the mask array - mask[i] is true if the
-  // character s[i] has been guessed.  The mask
-  // must be allocated, and initialized to all false
-  int N = strlen(answer);
-  int mask[N];
-  for (int i=0; i < N; ++i) {
-    mask[i] = 0;
-  }
+for(counter = 0; counter<26; counter++)
+    {
+        if(win==len)			   
+            printf("Good job! The word is \"%s\".\n", answer);
+            break;
+        }
+if(strikes==10)
+        {
+             printf("Game Over! The word is \"%s\".\n", answer);
+            break;
+	}
+printf("\n\n\n\n%s", stars);
+printf("\n\nGuess a letter:");
+        scanf(" %c",&ch);
+ for(i=0; i<len; i++)            		
+        {
+            if(word[i]==ch)
+            {
+                stars[i]=ch;		    		
+                flag++;			   	
+                wincounter++;		    		
+            }
+        }
 
-  // Loop over each round of guessing
-  int gameover = 0;
-  while (! gameover) {
-    // Print word with *s for unguessed letters
-    printf("The word is : ");
-    for(int j=0; j < N; ++j) {
-      if (mask[j]) {
-        printf("%c", answer[j]);
-      }
-      else {
-        printf("*");
-      }
+        if(flag==0)
+        {
+            strikes++;				    	
+
+        flag = 0;				    
     }
-    printf("\n");
-
-    // Get player's next guess
-    char guess;
-    printf("Letter? ");
-    fflush(stdout);
-    scanf(" %c", &guess);
-
-    // Mark true all mask positions corresponding to guess
-    for(int k=0; k < N; ++k) {
-      if (answer[k] == guess) {
-	mask[k] = 1;
-      }
-    }
-
-    // Determine whether the player has won!
-    gameover = 1;
-    for(int m = 0; m < N; ++m) {
-      if (!mask[m]) {
-        gameover = 0;
-        break;
-      }
-    }
-  }
-
-  // Print victory message!
-  printf("Victory! The word is \"%s\".\n", answer);
 
   return 0;
 }
